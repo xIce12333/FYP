513075612ae5ecfce6ffbed99195c6fd7cbef590
xIce <wlwilliam12333@gmail.com>
2022-06-23 14:19:11 +0800

    Commit Komachi
    Commit Komachi

--- Source/FYP/Private/Character/Komachi/MiraiKomachi.cpp
+++ Source/FYP/Private/Character/Komachi/MiraiKomachi.cpp
@@ -9,6 +9,9 @@ AMiraiKomachi::AMiraiKomachi()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
 	GetCharacterMovement()->bOrientRotationToMovement = true;
 
 }
