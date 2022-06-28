a6d1cab5278fb080fc4279ac6c21acf19f790620
xIce <wlwilliam12333@gmail.com>
2022-06-28 11:48:15 +0800

    commit Main Character
    commit Main Character

--- Source/FYP/Private/Character/Komachi/KomachiAnimController.cpp
+++ Source/FYP/Private/Character/Komachi/KomachiAnimController.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "KomachiAnimController.h"
+#include "Character/Komachi/KomachiAnimController.h"
 
 UKomachiAnimController::UKomachiAnimController()
 {
