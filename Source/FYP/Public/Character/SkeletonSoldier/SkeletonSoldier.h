b16b974471f29368a901c41cbdc023de78585165
xIce <wlwilliam12333@gmail.com>
2022-07-23 21:34:05 +0800

    add skeletonSoldier animation 23 July
    add skeletonSoldier animation 23 July

---
+++ Source/FYP/Public/Character/SkeletonSoldier/SkeletonSoldier.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/BaseClass/EnemyBase.h"
+#include "SkeletonSoldier.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FYP_API ASkeletonSoldier : public AEnemyBase
+{
+	GENERATED_BODY()
+	
+};
